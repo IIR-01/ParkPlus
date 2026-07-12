90271442f4f54eee15b2adc798b4e20553bf41b2
zayed-git <hassanmdzayed2023@gmail.com>
2026-07-12 18:23:01 +0600

    Sprint 1: Ride Wait Time Management

---
+++ backend/models/Ride.js
@@ -0,0 +1,19 @@
+const rides = [
+  {
+    id: 1,
+    name: "Roller Coaster",
+    waitTime: 20
+  },
+  {
+    id: 2,
+    name: "Ferris Wheel",
+    waitTime: 10
+  },
+  {
+    id: 3,
+    name: "Water Splash",
+    waitTime: 15
+  }
+];
+
+module.exports = rides;
